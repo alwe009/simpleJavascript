f2105b5561784f8269aabb6a3b3af500fd96cb89
Alexander Adolfo <alwe0009@gmail.com>
2022-07-23 17:32:39 +0800

    add confirm

--- main.js
+++ main.js
@@ -4,3 +4,8 @@
 let username = prompt("What is your name?", "type name here");
 
 alert(`Hi ${username}!`);
+
+
+let isUser = confirm(`Are you the ${username}?`);
+
+alert( isUser ); // true if OK is pressed
